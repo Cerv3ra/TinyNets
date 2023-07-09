8d704652f62fb2c1b3d9367e1d8fbe9a67be6363
Cerv3ra <new-email>
2023-07-09 17:50:17 +0100

    MNIST file

---
+++ MNIST.py
@@ -0,0 +1,19 @@
+import numpy as np
+import tinygrad
+import cv2
+
+
+#Load Model
+from tinygrad.state import safe_save, safe_load,get_state_dict, load_state_dict
+net = () #potential bug, do I have to define the whole structure again? 
+state_dict = safe_load("TinyDense.safetensors")
+net = get_state_dict
+#Load Image
+img = cv2.imread('3s.png')
+imglabel = 3 #Output expected
+#print(img.shape)
+
+
+print(net)
+
+#print val result
